14399fcce4fc2d1aa38a9e2c053e5cfc44ccca9d
Xun-un <4b0g0140@stust.edu.tw>
2023-11-09 11:58:13 +0800

    1109

---
+++ 1109-2.py
@@ -0,0 +1,9 @@
+def func1(*args):
+    for i in args:
+        print(i)
+
+# call function with 3 arguments
+func1(20, 40, 60)
+
+# call function with 2 arguments
+func1(80, 100)
--- 1109.py
+++ 1109.py
@@ -1,1 +1,7 @@
-print('text')
+# demo is the function name
+def demo(name, age):
+    # print value
+    print(name, age)
+
+# call function
+demo("Ben", 25)
